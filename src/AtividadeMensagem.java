fe4728723c20cb6b4ef37f0eb9e70d25ac74ae05
joaovictorcastro178 <joaovictorcastro178@gamil.com>
2026-04-17 21:52:58 -0300

    Início do Doors

--- src/AtividadeMensagem.java
+++ src/AtividadeMensagem.java
@@ -1,5 +1,31 @@
 
+import javax.swing.JOptionPane;
+
+//Crie um programa em Java utilizando a classe JOptionPane que faça o seguinte: 
+//
+//Encaminhe somente o link do GitHub!
+//
+//Solicite ao usuário que informe: 
+//Login 
+//Senha 
+//O sistema deve verificar: 
+//Login correto: admin 
+//Senha correta: 1313 
+//Se o login e a senha estiverem corretos: 
+//Mostrar a mensagem: "Login realizado com sucesso!" 
+//Caso contrário: 
+//Mostrar a mensagem: "Login ou senha inválidos." 
+//Utilize apenas caixas de diálogo (JOptionPane), não utilize System.out.println.
 
 public class AtividadeMensagem {
 
+    public static void main(String[] args) {
+       
+        JOptionPane.showMessageDialog(null, "Bem vindo ao Doors ");
+        
+        String nome = JOptionPane.showInputDialog("Informe seu nome: ");
+        JOptionPane.showMessageDialog(null, "Nome do usuário: "+nome);
+        
+        
+    }
 }
